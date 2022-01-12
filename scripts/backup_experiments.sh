6cde81f8277a798a20007c34dbe87005c60379ec
matt-lourens <matt.lourens.123@gmail.com>
2022-01-12 08:33:02 +0200

    backup script but will only work on my laptop

--- scripts/backup_experiments.sh
+++ scripts/backup_experiments.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-tar -zcvf ./experiments.tar.gz ./experiments
+# backup entire experiment folder and then remove tar file
+backup_date=date +'%Y%m%d'
+tar -zcvf ./experiment_$backup_date.tar.gz ./experiments
+cp experiment_$backup_date.tar.gz /mnt/c/Users/mattl/OneDrive/science/projects/qcnn/backup_experiments/
+rm experiment_$backup_date.tar.gz
